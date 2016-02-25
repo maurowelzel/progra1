7129058be980e9c09eade7ea99183be4774f8fd5
maurowelzel <skyers1997@hotmail.com>
2016-02-25 10:04:11 -0400

    Added files via upload

---
+++ sumaa.cpp
@@ -0,0 +1,20 @@
+// sumaa.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "conio.h"
+#include <iostream>
+using namespace std;4
+void main()
+
+{
+	int a,b,x;
+	cout<<"ingrese operador 1: ";
+	cin>> a;
+	cout<<"ingrese operador 2: ";
+	cin>>b;
+	x=a+b;
+	cout<< "la suma es :"<<x;
+		getch();
+}
+
